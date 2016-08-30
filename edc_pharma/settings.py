77a0a3417a916888df29ea1d0dfd05f008cb3850
Keletso Botsalano <kbotsalano@bhp.org.bw>
2016-08-30 08:37:19 +0200

    removing secret key from settings file

--- edc_pharma/settings.py
+++ edc_pharma/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w30a*vldb5m5_!6z5ls0shizm8p()738y8mhxb3k+8379sa&eh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
