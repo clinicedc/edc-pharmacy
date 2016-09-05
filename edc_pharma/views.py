740df769819b29ecb96069e09894a257cd2558d5
Keletso Botsalano <kbotsalano@bhp.org.bw>
2016-09-05 16:00:49 +0200

    using forms to print dispense data

--- edc_pharma/views.py
+++ edc_pharma/views.py
@@ -11,40 +11,51 @@ from django.http.response import HttpResponse
 from django.views.generic.base import TemplateView
 from edc_label.view_mixins import EdcLabelViewMixin
 
-
-from .models import Dispense, Patient, Treatment, Site, Protocol
+from .models import Dispense
 
 
 class HomeView(EdcBaseViewMixin, EdcLabelViewMixin, TemplateView):
 
     template_name = 'edc_pharma/home.html'
-    print_server_error = None
 
     paginate_by = 10
-
-    def __init__(self, **kwargs):
-        self._print_server = None
-        self._printers = {}
-        self.cups_server_ip = app_config.default_cups_server_ip
-        self.printer_label = app_config.default_printer_label
-        super(HomeView, self).__init__(**kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         subject_identifier = self.request.GET.get("subject_identifier")
         dispenses = Dispense.objects.filter(
             patient__subject_identifier=subject_identifier).order_by('date_prepared')
-        print(dispenses)
         context.update({'dispenses': dispenses})
         return context
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
+
+        if 'pk' in request.GET:
+            dispense_id = self.request.GET.get("pk")
+            dispense_data = Dispense.objects.get(pk=dispense_id)
+
+            context = {
+                'site': dispense_data.patient.site,
+                'telephone_number': dispense_data.patient.site.telephone_number,
+                'patient': dispense_data.patient.subject_identifier,
+                'initials': dispense_data.patient.initials,
+                'dosage': dispense_data.dose_amount,
+                'frequency': dispense_data.frequency_per_day,
+                'date_prepared': dispense_data.date_prepared,
+                'prepared_by': dispense_data.user_created,
+                'storage_instructions': dispense_data.treatment.storage_instructions,
+                'protocol': dispense_data.treatment.protocol
+            }
+
+            print(context)
+
+            context.update(context)
+            self.print_label("dispense_label")
+
+        else:
+            return self.render_to_response(context)
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(HomeView, self).dispatch(*args, **kwargs)
-    
-    
-
