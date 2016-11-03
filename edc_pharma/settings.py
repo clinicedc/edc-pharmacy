108740905f6780833e50271e9ece3d2ef9aa9652
Kagiso Motingwa <kagisomotingwa@gmail.com>
2016-11-03 14:38:24 +0200

    adding search on admin changelist

--- edc_pharma/settings.py
+++ edc_pharma/settings.py
@@ -91,13 +91,9 @@ WSGI_APPLICATION = 'edc_pharma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pharma',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'cc3721b'
-      }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
