aa3bec18be7bf5df1b3195b73f9c7b5134660530
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-10-20 12:31:14 +0200

    rename dispense_appointment_model to appointment_model

--- edc_pharma/apps.py
+++ edc_pharma/apps.py
@@ -14,7 +14,7 @@ class AppConfig(DjangoAppConfig):
     country = 'botswana'
     map_area = 'gaborone'
     prescription_model = None
-    dispense_appointment_model = 'edc_pharma.dispenseappointment'
+    appointment_model = 'edc_pharma.dispenseappointment'
     template_name = None
     holiday_csv_path = os.path.join(settings.BASE_DIR, 'holidays.csv')
     try:
