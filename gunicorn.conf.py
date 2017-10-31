3c6bc2db2b8e85c5c34c6a01d97a943b4b0c8935
erikvw <ew2789@gmail.com>
2017-10-31 20:50:52 +0200

    rename module

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,8 @@ import os
 SOURCE_ROOT = os.path.expanduser('~/source')
 
 bind = "127.0.0.1:9000"  # Don't use port 80 because nginx occupied it already.
-errorlog = os.path.join(SOURCE_ROOT, 'edc-pharma/logs/gunicorn-error.log')  # Make sure you create the log folder
-accesslog = os.path.join(SOURCE_ROOT, 'edc-pharma/logs/gunicorn-access.log')
+# Make sure you create the log folder
+errorlog = os.path.join(SOURCE_ROOT, 'edc-pharmacy/logs/gunicorn-error.log')
+accesslog = os.path.join(SOURCE_ROOT, 'edc-pharmacy/logs/gunicorn-access.log')
 loglevel = 'debug'
 workers = 1  # the number of recommended workers is '2 * number of CPUs + 1'
