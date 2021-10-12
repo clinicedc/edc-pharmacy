0458d9c4dbb547f087bc5f9f761eb8e27890787d
erikvw <ew2789@gmail.com>
2021-10-11 22:31:52 -0500

    add label template

--- edc_pharmacy/apps.py
+++ edc_pharmacy/apps.py
@@ -4,8 +4,5 @@ from django.apps import AppConfig as DjangoAppConfig
 class AppConfig(DjangoAppConfig):
     name = "edc_pharmacy"
     verbose_name = "Pharmacy"
-    # prescription_model = "edc_pharmacy.prescription"
-    # worklist_model = "edc_pharmacy.worklist"
-    # template_name = None
     has_exportable_data = True
     include_in_administration_section = True
