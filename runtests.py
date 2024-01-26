85f596e2e1743444af297d09906fd9a377698bfc
erikvw <ew2789@gmail.com>
2024-01-26 12:29:28 -0600

    move post-migrate, autodiscover and/or system checks to edc-appconfig

--- runtests.py
+++ runtests.py
@@ -3,7 +3,6 @@ import logging
 from pathlib import Path
 
 from dateutil.relativedelta import relativedelta
-from edc_constants.constants import IGNORE
 from edc_test_utils import DefaultTestSettings, func_main
 from edc_utils import get_utcnow
 
@@ -12,7 +11,7 @@ base_dir = Path(__file__).absolute().parent
 
 project_settings = DefaultTestSettings(
     calling_file=__file__,
-    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    SILENCED_SYSTEM_CHECKS=["sites.E101", "edc_navbar.E002", "edc_navbar.E003"],
     EDC_AUTH_CODENAMES_WARN_ONLY=True,
     EDC_AUTH_SKIP_SITE_AUTHS=True,
     EDC_AUTH_SKIP_AUTH_UPDATER=True,
@@ -27,9 +26,43 @@ project_settings = DefaultTestSettings(
         get_utcnow().replace(microsecond=999999, second=59, minute=59, hour=11)
         + relativedelta(years=6)
     ),
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "multisite",
+        "edc_action_item.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
+        "edc_data_manager.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+        "edc_form_runners.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_label.apps.AppConfig",
+        "edc_locator.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_offstudy.apps.AppConfig",
+        "edc_randomization.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_timepoint.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_prn.apps.AppConfig",
+        "edc_pdf_reports.apps.AppConfig",
+        "edc_pharmacy.apps.AppConfig",
+        "edc_appconfig.apps.AppConfig",
+    ],
     add_dashboard_middleware=True,
     add_lab_dashboard_middleware=True,
-    excluded_apps=["edc_adverse_event.apps.AppConfig", "adverse_event_app.apps.AppConfig"],
 ).settings
 
 
